39f2e1253a1fc9f3126ba49e706fc4121c023d39
jet-delossantos <59231126+jet-delossantos@users.noreply.github.com>
2022-02-17 11:28:14 +0800

    Added exposure of headers to frontend

--- repo/src/main/java/com/satellitedata/RepoApplication.java
+++ repo/src/main/java/com/satellitedata/RepoApplication.java
@@ -7,16 +7,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import com.satellitedata.service.impl.UserServiceImpl;
-
-import static com.satellitedata.constant.FileConstant.*;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static com.satellitedata.constant.FileConstant.*;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.satellitedata")
@@ -38,6 +39,21 @@ public class RepoApplication {
 		}
 	}
 	
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
 	
 	@Bean
 	public BCryptPasswordEncoder bCryptPPasswordEncoder() {
